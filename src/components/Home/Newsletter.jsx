47e3a9660d45f8a7e51ff05c5cc49e420ea598b5
Gjeev <g_jeevika@me.iitr.ac.in>
2023-12-22 14:04:51 +0530

    newsletter changes

--- src/components/Home/Newsletter.jsx
+++ src/components/Home/Newsletter.jsx
@@ -1,24 +1,23 @@
-import React from 'react'
-import newsletterImage from "../assets/newsletter.png" 
 
 const Newsletter = () => {
   return (
-    <div className='newsletter'>
+    <div className="newsletter">
       <div className="newsletter-left">
         <p>Third Edition of our </p>
         <p>Biannual Newsletter is out!</p>
         <h1>THE ASTRO INSIDER</h1>
-        <button className='read-here'>Read here &gt;&gt;</button>
+        <button className="read-here">Read here &gt;</button>
       </div>
       <div className="newsletter-right">
-        <div className="newsletter-img newsletter-img-border">
-          <img src={newsletterImage} alt="" />
+        <div className="newsletter-img-border">
+          <div className="newsletter-img">
+            {/* <img src={newsletterImage} alt="" /> */}
+          </div>
         </div>
-        <div className="newsletter-ellipse">
-        </div>
+        <div className="newsletter-ellipse"></div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Newsletter
+export default Newsletter;
