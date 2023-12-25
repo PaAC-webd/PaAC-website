40e32e1c101c2f78eeba1705654ede2df9388bd9
Gjeev <g_jeevika@me.iitr.ac.in>
2023-12-25 22:04:56 +0530

    updating stuff

--- src/components/People/People.jsx
+++ src/components/People/People.jsx
@@ -12,6 +12,30 @@ export default function People() {
       <div className="about-topbar">
         <Topbar />
       </div>
+      <div className="our-philo">
+        <div className="philo-title">
+          <h1 >Our Philosophy</h1>
+        </div>
+        <div className="philo-body">
+          <p className="philo-body-text">
+            Have you ever looked at the stars in your search meaning and turned
+            to science forever? Then welcome to PaAC! A vibrant hub fostering
+            scientific discourse and academic dialogue for the campus community.
+            While our primary focus spans Physics, Mathematics, and Astronomy,
+            we invite discussions aligned with the scientific method. As an
+            inclusive club, we thrive on student-driven initiatives. Your
+            suggestions for events are not just welcome; they are encouraged.
+            Whether you envision a lecture, workshop, or any academic endeavor,
+            reach out, and we will collaborate to make it happen! If you are
+            enthusiastic about science and academia and are willing to commit
+            yourself to organising and the behind-the-scenes work, join us on
+            our endeavours through space and time.
+          </p>
+        </div>
+      </div>
+      <div className="about-heading">
+        <h1 className="about-heading-text">Meet our team</h1>
+      </div>
 
       <div className="members">
         <div className="members-secys">
