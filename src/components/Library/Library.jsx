0d3eb8c4171845a9c0c66f1fc75d450a7943891e
vaibhav1me <vaibhav1@me.iitr.ac.in>
2023-12-26 13:31:46 +0530

    completed library section

--- src/components/Library/Library.jsx
+++ src/components/Library/Library.jsx
@@ -1,19 +1,50 @@
 import Topbar from "../Topbar/Topbar";
 import "./Library.css";
 import Footer from "../Footer/Footer";
+import {books as allBooks} from "./Data";
+import { useState } from "react";
+import BookCard from "../Gallery/Card";
+
 
 const Library = () => {
+  const [subject, setSubject] = useState('ALL')
+  const [books, setBooks] = useState(allBooks)
+
   return (
     <>
-      <div className="events-container">
-        <div className="events-topbar">
+      <div className="library-container">
+        <div className="library-topbar">
           <Topbar />
         </div>
-        <h1 className="events-heading-title">Library</h1>
-        <div className="events-recent">
-          <h1>Coming Soon</h1>
-        </div>
+        <section className="library">
+          <div className="library-top">
+            <h1>Library</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores provident nisi, nemo corrupti, ab commodi sint maxime natus rem ut praesentium soluta alias cupiditate neque consectetur ex quibusdam, in vero. Quisquam, laudantium! Vero consequatur aspernatur molestias maiores quam tempora sunt, veniam magni! Numquam laudantium ea provident similique eius minus quidem.</p>
+          </div>
+          <div className="library-bottom">
+            <div className="filter">
+              <span>Showing {subject} Books</span>
+              <div className="filter-menu">
+                  <span className="">Sort By:</span>
+                  <span className="filter-options">Physics</span>
+                  <span className="filter-options">Mathematics</span>
+                  <span className="filter-options">Astronomy</span>
+              </div>
+            </div>
+            <div className="books-container">
+              {
+                books.map((book) => {
+                  return (
+                    <BookCard {...book}/>
+                  )
+                })
+              }
+            </div>
+          </div>
+        </section>
+        <div className="footer">
         <Footer />
+        </div>
       </div>
     </>
   );
