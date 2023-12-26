989ea64de6fec012f5fd8b8de2577c4c62e96dfd
Gjeev <g_jeevika@me.iitr.ac.in>
2023-12-26 23:35:12 +0530

    chnages to openprojects

--- src/components/OpenProjects/OpenProjects.jsx
+++ src/components/OpenProjects/OpenProjects.jsx
@@ -1,7 +1,7 @@
 import "./OpenProjects.css";
 import Topbar from "../Topbar/Topbar";
 import Footer from "../Footer/Footer";
-import {openProjects} from './OpenData'
+import { openProjects } from "./OpenData";
 import ProjectCard from "./Card";
 
 export default function Events() {
@@ -12,14 +12,19 @@ export default function Events() {
       </div>
       <section className="open-projects">
         <h1>Open Projects</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, ea
+          reprehenderit, iste facere veniam expedita alias vero autem dolor
+          asperiores assumenda tempora odit blanditiis quae architecto quasi ad
+          facilis beatae. Lorem ipsum dolor sit amet, consectetur adipisicing
+          elit. Illum, ea reprehenderit, iste facere veniam expedita alias vero
+          autem dolor asperiores assumenda tempora odit blanditiis quae
+          architecto quasi ad facilis beatae.
+        </p>
         <div className="projects">
-            {
-                openProjects.map((project) => {
-                    return (
-                        <ProjectCard {...project} />
-                    )
-                })
-            }
+          {openProjects.map((project) => {
+            return <ProjectCard {...project} key={project.id} />;
+          })}
         </div>
       </section>
       <div className="footer">
