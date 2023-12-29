5ca2d0a67a4165a0d3f6ef946c2d2b3fcc510c8b
Gjeev <g_jeevika@me.iitr.ac.in>
2023-12-29 22:01:00 +0530

    adding links

--- src/components/Events/EventCard.jsx
+++ src/components/Events/EventCard.jsx
@@ -1,26 +1,22 @@
-import { CalendarIcon, GlobeIcon } from "@radix-ui/react-icons";
+import { CalendarIcon } from "@radix-ui/react-icons";
 import "./Events.css";
 
 export default function EventCard(props) {
-  // const encodedTitle = encodeURIComponent(props.title);
+  const encodedTitle = encodeURIComponent(props.title);
   return (
     <div className="recent-event">
       <div>
-        <h2 className="recent-event-time">
-          <CalendarIcon /> {props.date}
-        </h2>
         <img src={props.image} alt="" className="recent-event-image" />
         <h1 className="recent-event-title">{props.title}</h1>
         <div className="recent-divider" />
       </div>
-      {/* <h2 className="event-location">
-          <GlobeIcon/> {props.location}
-        </h2> */}
+      <h2 className="recent-event-time">
+        <CalendarIcon /> {props.date}
+      </h2>
       <div>
         <p className="recent-event-details">{props.details}</p>
         <button className="recent-event-share-button">
-          {/* <a href={`/event/${props.title}`}>Learn more</a> */}
-          Details
+          <a href={`/event/${encodedTitle}`}>Learn more</a>
         </button>
       </div>
     </div>
