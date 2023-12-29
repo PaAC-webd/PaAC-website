a3a625b32852f415029e74a88be41a423df848ed
vaibhav1me <vaibhav1@me.iitr.ac.in>
2023-12-29 21:47:28 +0530

    completed previous events

--- src/components/Events/EventCard.jsx
+++ src/components/Events/EventCard.jsx
@@ -1,25 +1,28 @@
+import { CalendarIcon, GlobeIcon } from "@radix-ui/react-icons";
 import "./Events.css";
 
 export default function EventCard(props) {
-  const encodedTitle = encodeURIComponent(props.title);
+  // const encodedTitle = encodeURIComponent(props.title);
   return (
-    <div className="EventCard-container">
-      <h1 className="event-title">{props.title}</h1>
-      <div className="event-info">
-        <h2 className="event-time">
-          {" "}
-          <i className="icon-clock"></i> {props.date}
+    <div className="recent-event">
+      <div>
+        <h2 className="recent-event-time">
+          <CalendarIcon /> {props.date}
         </h2>
-        <h2 className="event-location">
-          {" "}
-          <i className="icon-location"></i> {props.location}
-        </h2>
-        <hr className="divider" />
-        <p className="event-details">{props.details}</p>
+        <img src={props.image} alt="" className="recent-event-image" />
+        <h1 className="recent-event-title">{props.title}</h1>
+        <div className="recent-divider" />
       </div>
-      <button className="event-share-button">
-        <a href={`/event/${encodedTitle}`}>Learn more</a>
-      </button>
+      {/* <h2 className="event-location">
+          <GlobeIcon/> {props.location}
+        </h2> */}
+      <div>
+        <p className="recent-event-details">{props.details}</p>
+        <button className="recent-event-share-button">
+          {/* <a href={`/event/${props.title}`}>Learn more</a> */}
+          Details
+        </button>
+      </div>
     </div>
   );
 }
