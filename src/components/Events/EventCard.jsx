fdd5f8497a5f1907624d48d5d6cc3c30f94774c0
Gjeev <g_jeevika@me.iitr.ac.in>
2024-01-05 22:28:23 +0530

    css changes

--- src/components/Events/EventCard.jsx
+++ src/components/Events/EventCard.jsx
@@ -4,22 +4,27 @@ import "./Events.css";
 export default function EventCard(props) {
   const encodedTitle = encodeURIComponent(props.title);
   return (
-    <div className="recent-event">
-      <div className="recent-top">
-        <h1 className="recent-event-title">{props.title}</h1>
-        <img src={props.image} alt="" className="recent-event-image" />
-        <h2 className="recent-event-time">
-          <CalendarIcon /> {props.date}
-        </h2>
+    <a
+      href={`/event/${encodedTitle}`}
+      target="_blank"
+      rel="noreferrer"
+    >
+      <div className="recent-event">
+        <div className="recent-top">
+          <img src={props.image} alt="" className="recent-event-image" />
+          <h1 className="recent-event-title">{props.title}</h1>
+          <h2 className="recent-event-time">
+            <CalendarIcon /> {props.date}
+          </h2>
+        </div>
+        <div className="recent-divider" />
+        <div className="recent-bottom">
+          <p className="recent-event-details">{props.details}</p>
+        </div>
+        <a href={`/event/${encodedTitle}`} className="learn-button">
+          Learn more
+        </a>
       </div>
-
-      <div className="recent-divider" />
-      <div className="recent-bottom">
-        <p className="recent-event-details">{props.details}</p>
-        <button className="recent-event-share-button">
-          <a href={`/event/${encodedTitle}`}>Learn more</a>
-        </button>
-      </div>
-    </div>
+    </a>
   );
 }
