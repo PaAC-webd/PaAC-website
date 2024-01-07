258892f7f8f5b99c74021ad9ba4b3f59276a964d
critac4 <77203338+critac4@users.noreply.github.com>
2024-01-07 17:10:58 +0530

    Update items.js
    Added FAQ Answers on open projects page

--- src/components/OpenProjects/items.js
+++ src/components/OpenProjects/items.js
@@ -2,22 +2,22 @@ export const items = [
     {
         id: 1,
         question: "What are Open Projects?",
-        answer: "lorem lorem lorem",
+        answer: "Open Projects are Projects that PaAC is working on currently. The Projects are from a variety of fields including Physics, Mathematics, Machine Learning and Data Science, Electronics and many more! We invite everyone to join the projects and gain a certificate on completion.",
     },
     {
         id: 2,
         question: "When are the projects released?",
-        answer: "lorem lorem lorem",
+        answer: "The Projects are released biannually, at the start every semester, barring a few delays.",
     },
     {
         id: 3,
         question: "Who are eligible?",
-        answer: "lorem lorem lorem",
+        answer: "All the students of IIT Roorkee are eligible to join a project, or even host one of their own!!",
     },
     {
         id: 4,
         question: "How can I apply?",
-        answer: "lorem lorem lorem",
+        answer: "When the projects are released, a google form is released with the same, when we are taking students, the link will be made available here too!",
     },
 
 ];
