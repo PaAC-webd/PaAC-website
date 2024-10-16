d644a8403ac4771d720790277963205025d3292c
Gjeev <g_jeevika@me.iitr.ac.in>
2024-10-16 16:28:33 +0530

    revert to old commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import React, { lazy, Suspense } from "react";
-const Home = lazy(() => import("./components/Home/home"))
-const Events = lazy(() => import("./components/Events/Events"))
+import React from "react";
+import Home from "./components/Home/home";
+import Events from "./components/Events/Events";
 import Member from "./components/People/People";
 import Gallery from "./components/Gallery/Gallery";
 import Contact from "./components/Contact/Contact";
@@ -9,14 +9,15 @@ import Library from "./components/Library/Library";
 import OpenProjects from "./components/OpenProjects/OpenProjects";
 import Layout from "./components/Layout";
 
+
 function App() {
   const router = createBrowserRouter([
     {
-      path: "/",
+      path: '/',
       element: <Layout />,
       children: [
-        { index: true, element: <Suspense fallback={"Loading..."}><Home /></Suspense> },
-        { path: "/events", element: <Suspense fallback={"Loading..."}><Events /></Suspense> },
+        { index: true, element: <Home /> },
+        { path: "/events", element: <Events /> },
         { path: "/people", element: <Member /> },
         { path: "/gallery", element: <Gallery /> },
         { path: "/contact-us", element: <Contact /> },
