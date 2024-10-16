95724ee684e9f6fd05fdec79629ec179ce1601f6
vaibhav1me <vaibhav1@me.iitr.ac.in>
2024-10-16 15:17:55 +0530

    changed image and lazy loaded some components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import Home from "./components/Home/home";
-import Events from "./components/Events/Events";
+import React, { lazy, Suspense } from "react";
+const Home = lazy(() => import("./components/Home/home"))
+const Events = lazy(() => import("./components/Events/Events"))
 import Member from "./components/People/People";
 import Gallery from "./components/Gallery/Gallery";
 import Contact from "./components/Contact/Contact";
@@ -9,15 +9,14 @@ import Library from "./components/Library/Library";
 import OpenProjects from "./components/OpenProjects/OpenProjects";
 import Layout from "./components/Layout";
 
-
 function App() {
   const router = createBrowserRouter([
     {
       path: '/',
       element: <Layout />,
       children: [
-        { index: true, element: <Home /> },
-        { path: "/events", element: <Events /> },
+        { index: true, element: <Suspense fallback={"Loading..."}><Home /></Suspense> },
+        { path: "/events", element: <Suspense fallback={"Loading..."}><Events /></Suspense> },
         { path: "/people", element: <Member /> },
         { path: "/gallery", element: <Gallery /> },
         { path: "/contact-us", element: <Contact /> },
