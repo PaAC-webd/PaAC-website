6a7bcfdcd46adaa5d1173b46ddc2fe37f8405821
vaibhav1me <vaibhav1@me.iitr.ac.in>
2024-12-14 11:49:45 +0530

    solved the captions issue

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,16 +1,17 @@
 import { useEffect, useState } from "react";
 import "./Gallery.css";
 import { data } from "./Images.js";
-import "lightbox.js-react/dist/index.css";
-import { SlideshowLightbox, initLightboxJS } from "lightbox.js-react";
+import "lightbox.js-react";
+import { SlideshowLightbox} from "lightbox.js-react";
 import Masonry from "react-responsive-masonry";
 
 export default function Gallery() {
   const [isOpen, setIsOpen] = useState(false);
-  const [windowWidth, setWindowWitdth] = useState(window.innerWidth);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [slideIndex, setSlideIndex] = useState(0);
 
   const handleResize = () => {
-    setWindowWitdth(window.innerWidth);
+    setWindowWidth(window.innerWidth);
   };
 
   useEffect(() => {
@@ -20,76 +21,25 @@ export default function Gallery() {
       behavior: "smooth",
     });
     window.addEventListener("resize", handleResize);
-    initLightboxJS(import.meta.env.VITE_LIGHTBOX_LICENSE_KEY, "Individual");
 
     return () => {
       window.removeEventListener("resize", handleResize);
     };
   }, []);
 
-  useEffect(() => {
-    if (isOpen) {
-      setTimeout(() => {
-        document.getElementById("closeBtn").addEventListener("click", () => {
-          setIsOpen(false);
-        });
-      }, 1000);
-    }
-  }, [isOpen]);
-
   return (
     <>
-      {/* <div className="gallery_container">
-        <SlideshowLightbox
-          backgroundColor="#050528"
-          className="lightBox"
-          theme="night"
-          open={isOpen}
-          fullScreen={true}
-          showThumbnails={true}
-          imgAnimation="imgMotion"
-          modalClose="clickOutside"
-        >
-          {data.map((image, index) => {
-            return (
-              <img
-                className="gallery_image"
-                key={index}
-                src={image.image}
-                alt="gallery"
-              />
-            );
-          })}
-        </SlideshowLightbox>
-      </div>
-
-      <div className="masonry" onClick={() => {setIsOpen(!isOpen);}}>
-        <ResponsiveMasonry columnsCountBreakPoints={{ 350: 2, 500: 3 }}>
-          <Masonry gutter="10px">
-            {data.map((image, index) => {
-              return (
-                <img
-                title="Click for slideshow"
-                  key={index}
-                  src={image.image}
-                  alt={image.caption}
-                  style={{ width: "100%", display: "block" }}
-                />
-              );
-            })}
-          </Masonry>
-        </ResponsiveMasonry>
-      </div> */}
-
-      <SlideshowLightbox showThumbnails={true} images={data} lightboxIdentifier="lbox1">
+      <SlideshowLightbox showThumbnails={true} images={data} lightboxIdentifier="lbox1" open={isOpen} startingSlideIndex={slideIndex} onClose={() => {setIsOpen(false)}}>
         <Masonry columnsCount={windowWidth > 600 ? 3 : 2} gutter="10px">
           {data.map((image, i) => (
-            <div key={i}>
+            <div key={i}
+            >
             <img
               title="Click for slideshow"
               key={i}
-              src={image.image}
-              data-lightboxjs="lbox1"
+              onClick={() => {console.log("click", i); setSlideIndex(i); setIsOpen(true);}}
+              src={image.src}
+              // data-lightboxjs="lbox1"
               style={{ width: "100%", display: "block" }}
               />
               <p>{image.caption}</p>
